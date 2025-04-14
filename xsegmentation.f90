8c811b480feb59e2a4f508cbf2a44718ec935555
Beliavsky <beliavsky@aol.com>
2025-04-14 11:38:25 -0400

    passes xsq = x**2 to subroutines

--- xsegmentation.f90
+++ xsegmentation.f90
@@ -11,10 +11,10 @@ program xsegmentation
   use random_mod      , only: random_normal
   implicit none
 
-  integer, parameter :: n = 200    ! # of observations
+  integer, parameter :: n = 400    ! # of observations
   integer, parameter :: k_true = 2 ! True number of change points in the simulation
   integer, parameter :: k_max = 3  ! Maximum number of change points to test
-  real(kind=dp) :: x(n)
+  real(kind=dp) :: x(n), xsq(n)
   integer :: i, k, nseg
   integer, parameter :: cp_true(k_true) = [30, 70]
   real(kind=dp), parameter :: sigma(k_true+1) = [1.0_dp, 5.0_dp, 0.2_dp]
@@ -22,7 +22,7 @@ program xsegmentation
   ! For storing the estimated change points for each model with k>0
   integer, allocatable :: cp_est(:)
   real(kind=dp) :: best_ll, ll, sumsq
-  logical, parameter :: print_times = .false.
+  logical, parameter :: print_times = .true.
 
   do i = 1, n
      if (i <= cp_true(1)) then
@@ -33,6 +33,7 @@ program xsegmentation
         x(i) = sigma(3) * random_normal()
      end if
   end do
+  xsq = x**2
   print "(/,a,*(1x,i0))", "True Change Points (indices):", cp_true
   print "(/,a)", " k   Log-Likelihood   Detected Change Points"
   ! Loop over models with k = 0 to k_max change points.
@@ -43,12 +44,12 @@ program xsegmentation
      if (k == 0) then
         ! No change points: the model has one segment spanning the whole data.
         nseg = n
-        sumsq = sum(x(1:n)**2)
+        sumsq = sum(xsq)
         if (sumsq <= 0.0_dp) sumsq = 1.0e-10_dp
         ll = -0.5_dp * nseg * (1.0_dp + log(sumsq/nseg))
         write(*,"(I2,3X,F12.4)") k, ll
      else
-        call find_change_points(x, cp_est(:k), best_ll)
+        call find_change_points(xsq, cp_est(:k), best_ll)
         write(*,"(I2,3X,F12.4,3X,*(i4))") k, best_ll, cp_est(:k)
      end if
   end do
