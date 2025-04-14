b2bad67797f73d3ad398872bbeaab35922ac0eb7
Beliavsky <beliavsky@aol.com>
2025-04-14 11:13:13 -0400

    prints elapsed time vs. # of changepoints

--- xsegmentation.f90
+++ xsegmentation.f90
@@ -11,19 +11,18 @@ program xsegmentation
   use random_mod      , only: random_normal
   implicit none
 
-  integer, parameter :: n = 100
-  integer, parameter :: k_true = 2       ! True number of change points in the simulation
-  integer, parameter :: k_max = 4        ! Maximum number of change points to test
+  integer, parameter :: n = 200    ! # of observations
+  integer, parameter :: k_true = 2 ! True number of change points in the simulation
+  integer, parameter :: k_max = 3  ! Maximum number of change points to test
   real(kind=dp) :: x(n)
-  integer :: i, k
-
-  integer :: cp_true(k_true) = [30, 70]
-  real(kind=dp), allocatable :: sigma(:)
+  integer :: i, k, nseg
+  integer, parameter :: cp_true(k_true) = [30, 70]
+  real(kind=dp), parameter :: sigma(k_true+1) = [1.0_dp, 5.0_dp, 0.2_dp]
+  real(kind=dp), allocatable :: times(:)
   ! For storing the estimated change points for each model with k>0
   integer, allocatable :: cp_est(:)
   real(kind=dp) :: best_ll, ll, sumsq
-  integer :: nseg
-  sigma = [1.0_dp, 5.0_dp, 0.2_dp]
+  logical, parameter :: print_times = .false.
 
   do i = 1, n
      if (i <= cp_true(1)) then
@@ -34,28 +33,19 @@ program xsegmentation
         x(i) = sigma(3) * random_normal()
      end if
   end do
-
-  ! Print the true change points.
-  print *
-  print *, "True Change Points (indices):"
-  do i = 1, k_true
-     print *, cp_true(i)
-  end do
-  print *
-
-  ! Print table header.
-  print *, " k   Log-Likelihood     Detected Change Points"
-  print *, "-----------------------------------------------------"
-
+  print "(/,a,*(1x,i0))", "True Change Points (indices):", cp_true
+  print "(/,a)", " k   Log-Likelihood   Detected Change Points"
   ! Loop over models with k = 0 to k_max change points.
+  if (print_times) allocate (times(0:k_max+1))
   do k = 0, k_max
+     if (print_times) call cpu_time(times(k))
      if (k == 0) then
         ! No change points: the model has one segment spanning the whole data.
         nseg = n
         sumsq = sum(x(1:n)**2)
         if (sumsq <= 0.0_dp) sumsq = 1.0e-10_dp
         ll = -0.5_dp * nseg * (1.0_dp + log(sumsq/nseg))
-        write(*,'(I2,3X,F12.4,5X,A)') k, ll, "None"
+        write(*,"(I2,3X,F12.4,5X,A)") k, ll
      else
         allocate(cp_est(k))
         call find_change_points(x, cp_est, best_ll)
@@ -68,5 +58,9 @@ program xsegmentation
         deallocate(cp_est)
      end if
   end do
-
+  if (print_times) then
+     call cpu_time(times(k))
+     print "(/,a20, *(i8))", "#changepoints", (k, k=0, k_max)
+     print "(a20,*(f8.4))", "times", times(1:) - times(:k_max)
+  end if
 end program xsegmentation
